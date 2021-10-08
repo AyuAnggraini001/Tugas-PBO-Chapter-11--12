3eda4bcbf738df98bf66fb59b9a31acd97151666
AyuAnggraini001 <92178628+AyuAnggraini001@users.noreply.github.com>
2021-10-09 01:07:36 +0700

    tes polymorphsme
    Nama : Ayu Anggraini
    NIM : 20051214001
    Kelas : SI A 2020
    
    Program Studi Sistem Informasi 
    Jurusan Teknik Informatika
    Fakultas Teknik 
    Universitas Negeri Surabaya

--- Program.cs
+++ Program.cs
@@ -1,114 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 
-public abstract class Karyawan
+public class KomisiKaryawan
 {
     public string NamaAwal { get; }
     public string NamaAkhir { get; }
     public string KTP { get; }
-    public Karyawan (string Namadepan, string Namabelakang, string ktp)
+    private decimal penjualanKotor;
+    private decimal Tingkatkomisi;
+    public KomisiKaryawan(string Namadepan, string Namabelakang, string ktp, decimal penjualanKotor, decimal Tingkatkomisi)
     {
         NamaAwal = Namadepan;
         NamaAkhir = Namabelakang;
         KTP = ktp;
-    }
-    public override string ToString() => $"{NamaAwal}{NamaAkhir}\n" + $"No,KTP: {KTP}";
-    public abstract decimal Pendapatan();
-}
-public class GajiKaryawan : Karyawan
-{
-    private decimal Gajimingguan;
-    public GajiKaryawan (string Namadepan, string Namabelakang, string ktp, decimal Gajimingguan)
-        : base(Namadepan, Namabelakang, ktp)
-    {
-        GajiMingguan = Gajimingguan;
-    }
-    public decimal GajiMingguan
-    {
-        get
-        {
-            return Gajimingguan;
-        }
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    value, $"{nameof(GajiMingguan)} harus >= 0");
-            }
-            Gajimingguan = value;
-        }
-    }
-    public override decimal Pendapatan() => GajiMingguan;
-    public override string ToString() => $"Gaji Karyawan: {base.ToString()}\n" +
-        $"Gaji Mingguan: {GajiMingguan:C}";
-}
-public class KaryawanPerjam : Karyawan
-{
-    private decimal upah;
-    private decimal jam;
-    public KaryawanPerjam(string Namadepan, string Namabelakang, string ktp,
-        decimal Upahperjam, decimal jamKerja)
-        : base(Namadepan, Namabelakang, ktp)
-    {
-        Upah = Upahperjam;
-        Jam = jamKerja;
-    }
-    public decimal Upah
-    {
-        get
-        {
-            return upah;
-        }
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    value, $"{nameof(Upah)} harus >= 0");
-            }
-            upah = value;
-        }
-    }
-    public decimal Jam
-    {
-        get
-        {
-            return jam;
-        }
-        set
-        {
-            if (value < 0 || value > 168)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Jam)} harus >= 0 dan <= 168");
-            }
-            jam = value;
-        }
-    }
-    public override decimal Pendapatan()
-    {
-        if (Jam <= 40)
-        {
-            return Upah * Jam;
-        }
-        else
-        {
-            return (40 * Upah) + ((Jam - 40) * Upah * 1.5M);
-        }
-    }
-    public override string ToString() => $"Karyawan Perjam: {base.ToString()}\n" +
-        $"Upah Perjam: {Upah:C}\nJam Kerja: {Jam:F2}";
-}
-public class KomisiKaryawan : Karyawan
-{
-    private decimal penjualanKotor;
-    private decimal tingkatKomisi;
-    public KomisiKaryawan(string Namadepan, string Namabelakang,
-        string ktp, decimal penjualanKotor, decimal tingkatKomisi)
-        :base(Namadepan, Namabelakang, ktp)
-    {
         PenjualanKotor = penjualanKotor;
-        TingkatKomisi = tingkatKomisi;
+        TingkatKomisi = Tingkatkomisi;
     }
     public decimal PenjualanKotor
     {
@@ -118,10 +23,9 @@ public class KomisiKaryawan : Karyawan
         }
         set
         {
-            if (value < 0)
+            if (value < 0) 
             {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    value, $"{nameof(PenjualanKotor)} harus >= 0");
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(PenjualanKotor)} harus >= 0");
             }
             penjualanKotor = value;
         }
@@ -130,29 +34,27 @@ public class KomisiKaryawan : Karyawan
     {
         get
         {
-            return tingkatKomisi;
+            return Tingkatkomisi;
         }
         set
         {
             if (value <= 0 || value >= 1)
             {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    value, $"{nameof(TingkatKomisi)} harus > 0 dan < 1");
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(TingkatKomisi)} harus > 0 dan < 1"); 
             }
-            tingkatKomisi = value;
+            Tingkatkomisi = value;
         }
     }
-    public override decimal Pendapatan() => TingkatKomisi * PenjualanKotor;
-    public override string ToString() => $"Karyawan Komisi: {base.ToString()}\n" +
-        $"Penjualan Kotor: {PenjualanKotor:C}\n" +
-        $"Tingkat Komisi: {TingkatKomisi:F2}";
+    public virtual decimal Pendapatan() => TingkatKomisi * PenjualanKotor;
+    public override string ToString() => $"Karyawan Komisi: {NamaAwal}{NamaAkhir}\n" + 
+        $"No.KTP: {KTP}\n" + $"Penjualan Kotor: {PenjualanKotor:C}\n" + $"Tingkat Komisi: {TingkatKomisi:F2}";
 }
 public class KaryawanDasarKomisi : KomisiKaryawan
 {
     private decimal gajiPokok;
-    public KaryawanDasarKomisi(string Namadepan, string Namabelakang, string ktp,
-        decimal penjualanKotor, decimal tingkatKomisi, decimal gajiPokok)
-        : base(Namadepan, Namabelakang, ktp, penjualanKotor, tingkatKomisi)
+    public KaryawanDasarKomisi(string Namadepan, string Namabelakang, string ktp, decimal penjualanKotor, 
+        decimal Tingkatkomisi, decimal gajiPokok)
+        : base(Namadepan, Namabelakang, ktp, penjualanKotor, Tingkatkomisi)
     {
         GajiPokok = gajiPokok;
     }
@@ -166,44 +68,33 @@ public class KaryawanDasarKomisi : KomisiKaryawan
         {
             if (value < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    value, $"{nameof(GajiPokok)} harus >= 0");
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GajiPokok)} harus >= 0");
             }
             gajiPokok = value;
         }
     }
-    public override decimal Pendapatan() => GajiPokok * base.Pendapatan();
-    public override string ToString() => $"Gaji-Pokok: {base.ToString()}\nGaji Pokok: {GajiPokok:C}";
+    public override decimal Pendapatan() => GajiPokok + base.Pendapatan();
+    public override string ToString() => $"Gaji-Pokok {base.ToString()}\nGaji Pokok {GajiPokok:C}";
 }
-class tes_SistemPenggajian
+class tes_Polymorphism 
 {
     static void Main()
     {
-        var gajiKaryawan = new GajiKaryawan("John ", "Smith", "111-11-1111", 800.00M);
-        var Karyawanperjam = new KaryawanPerjam("Karen ", "Price", "222-22-2222", 16.75M, 40.0M);
-        var komisiKaryawan = new KomisiKaryawan("Sue ", "Jones", "333-33-3333", 10000.00M, .06M);
-        var karyawandasarkomisi = new KaryawanDasarKomisi("Bob ", "Lewts", "444-44-4444", 5000.00M, .04M, 300.00M);
-        Console.WriteLine("Karyawan diproses secara Induvidu:\n");
-        Console.WriteLine($"{gajiKaryawan}\ndiperoleh: " + $"{gajiKaryawan.Pendapatan():C}\n");
-        Console.WriteLine($"{Karyawanperjam}\ndiperoleh: "+ $"{Karyawanperjam.Pendapatan():C}\n");
-        Console.WriteLine($"{komisiKaryawan}\ndiperoleh: "+$" {komisiKaryawan.Pendapatan():C}\n");
-        Console.WriteLine($"{karyawandasarkomisi}\ndiperoleh: " + $"{karyawandasarkomisi.Pendapatan():C}\n");
-        var karyawan = new List<Karyawan>() { gajiKaryawan, Karyawanperjam, komisiKaryawan, karyawandasarkomisi };
-        Console.WriteLine("Karyawan diproses secara Pholymorphically:\n");
-        foreach(var Karyawansekarang in karyawan)
-        {
-            Console.WriteLine(Karyawansekarang);
-            if(Karyawansekarang is KaryawanDasarKomisi)
-            {
-                var karyawann = (KaryawanDasarKomisi) Karyawansekarang;
-                karyawann.GajiPokok *= 1.10M;
-                Console.WriteLine("Gaji Pokok baru dengan kenaikan 10% : " + $"{karyawann.GajiPokok:C}");
-            }
-            Console.WriteLine($"Diperoleh: {Karyawansekarang.Pendapatan():C}\n");
-        }
-        for (int j = 0; j < karyawan.Count; j++)
-        {
-            Console.WriteLine($"Karyawan {j} adalah {karyawan[j].GetType()}");
-        }
+        var komisiKaryawan = new KomisiKaryawan("Sue ", "Jones", "222-22-2222", 10000.00M, .06M);
+        var karyawandasarkomisi = new KaryawanDasarKomisi("Bob ", "Lewis", "333-33-3333", 5000.00M, .04M, 300.00M);
+        Console.WriteLine("Memanggil KomisiKaryawan dengan ToString dan metode pendapatan" + 
+            "dengan referensi class-base ke objek class base");
+        Console.WriteLine(komisiKaryawan.ToString());
+        Console.WriteLine($"Pendapatan: {komisiKaryawan.Pendapatan()}\n");
+        Console.WriteLine("Memanggil KaryawanDasarKomisi dengan ToString dan" + 
+            "method Pendapatan dengan turunan referensi untuk" + "class turunan objek");
+        Console.WriteLine(karyawandasarkomisi.ToString());
+        Console.WriteLine($"Pendapatan: {karyawandasarkomisi.Pendapatan()}\n");
+        KomisiKaryawan karyawankomisi2 = karyawandasarkomisi;
+        Console.WriteLine("Memanggil KaryawanDasarKomisi dengan ToString dan Pendapatan" + 
+            "method dengan class dasar untuk turunan-class objek");
+        Console.WriteLine(karyawankomisi2.ToString());
+        Console.WriteLine($"Pendapatan: {karyawandasarkomisi.Pendapatan()}\n");
     }
 }
+
